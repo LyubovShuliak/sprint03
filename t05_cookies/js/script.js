d31b0e5f6dfa850f0829d91fd7018fbcb6debf92
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2021-08-18 06:12:02 +0300

    tasks

---
+++ t05_cookies/js/script.js
@@ -0,0 +1,59 @@
+// function makeCounter() {
+//   let count = 0
+//   return function () {
+//     return count++
+//   }
+// }
+
+// let counter = makeCounter()
+// function doSave() {
+//   makeCounter()
+//   //Set the item in doSave()
+//   localStorage.setItem(counter(), text.value)
+//   let outtext = Object.values({ ...localStorage })
+//   console.log(outtext);
+//   let output = document.getElementById("cookies")
+//   output.textContent += localStorage.getItem(`${counter()}`)
+// }
+
+// //When the window loads...
+// window.onload = function () {
+//   saveButton = document.querySelector(".Show")
+//   saveButton.onclick = doSave
+
+//   textarea = document.getElementById("text")
+// }
+
+const elementBtn = document.querySelector('.Show');
+const elementUl = document.querySelector('ul');
+
+function updateStorage() {
+  const data = [];
+  for (let element of elementUl.querySelectorAll('li')) {
+    data.push(element.textContent);
+  }
+  localStorage['items'] = JSON.stringify(data);
+}
+
+function updateUl(items) {
+  const html = [];
+  for (let item of items) {
+    html.push(`<li>-->${localStorage.getItem(item)}</li>`);
+  }
+  elementUl.innerHTML = html.join('');
+}
+
+elementBtn.onclick = () => {
+  const elementsLi = elementUl.querySelectorAll('li');
+  const newLi = document.createElement('li');
+  newLi.textContent = localStorage.getItem(item);
+  elementUl.append(newLi);
+  updateStorage();
+}
+
+window.onstorage = event => {
+    saveButton = document.querySelector(".Show")
+      saveButton.onclick = doSave
+    
+      textarea = document.getElementById("text")
+}
---
+++ t07_words/js/script.js
@@ -0,0 +1,49 @@
+function addWords(obj, wrds) {
+  let array_of_wrds = wrds.match(/\w+/g)
+  let unique_words = obj.words.match(/\w+/g)
+  for (let index = 0; index < array_of_wrds.length; index++) {
+    unique_words.push(array_of_wrds[index])
+  }
+  let set = [...new Set(unique_words)].join(" ")
+  obj.words = set
+  return obj
+}
+function removeWords(obj, wrds) {
+  let array_of_wrds = wrds.match(/\w+/g)
+  let unique_words = obj.words.match(/\w+/g)
+  let returned = []
+  for (let index = 0; index < unique_words.length; index++) {
+    if (array_of_wrds.indexOf(unique_words[index]) == -1) {
+      returned.push(unique_words[index])
+    }
+  }
+  let set = [...new Set(returned)].join(" ")
+  obj.words = set
+  return obj
+}
+function changeWords(obj, oldWrds, newWrds) {
+  let array_of_oldWrds = oldWrds.match(/\w+/g)
+
+  let unique_words = obj.words.match(/\w+/g)
+  let returned = unique_words.map((x) => {
+    for (let index = 0; index < array_of_oldWrds.length; index++) {
+      if (array_of_oldWrds.indexOf(x) !== -1) {
+        return newWrds
+      } else {
+        return x
+      }
+    }
+  })
+
+  let set = [...new Set(returned)].join(" ")
+  obj.words = set
+  return obj
+}
+
+const obj = { words: "newspapers newspapers  books magazines" }
+console.log(obj)
+addWords(obj, "radio newspapers   ")
+removeWords(obj, "newspapers   radio")
+console.log(obj)
+changeWords(obj, "books radio  magazines", "tv internet")
+console.log(obj)
